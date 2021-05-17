7d073eb48cb630a4484c7efedf711c37c5206baa
Jah-On <58399643+Jah-On@users.noreply.github.com>
2021-05-16 22:50:35 -0500

    Create Connect4-OTA.py

---
+++ Connect4-OTA.py
@@ -0,0 +1,6 @@
+# Over-the-air file that retrieve the game from GitHub
+import urllib.request
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+exec(urllib.request.urlopen("https://raw.githubusercontent.com/Jah-On/Ultimate-Connect-4/main/Connecct4.py").read())
